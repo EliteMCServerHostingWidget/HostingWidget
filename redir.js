595964a0e7d389d1dec46ea561658149a9efa368
mwear3robloxandroid <mitchellwears@gmail.com>
2017-10-11 17:00:37 -0700

    Update redir.js

--- redir.js
+++ redir.js
@@ -1,6 +1,6 @@
 <script>
 // Check if the condition is true and then redirect.
 if ( ... ) {
-  window.location.href = "https://www.example.com";
+  window.location.href = "https://www.elitemchosting.tk";
 }
 </script>
